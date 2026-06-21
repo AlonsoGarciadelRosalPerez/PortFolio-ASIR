f15c97f9000f7bb45b1143f21b2a5e420ddbf403
Alonso García del Rosal Pérez <alonsogarciadelrosal@hotmail.com>
2026-06-21 23:13:06 +0200

    Add files via upload

---
+++ Programacion_Python/Calculadora.py
@@ -0,0 +1,24 @@
+'''Explicación de la calculadora'''
+# Se trata de escribir una aplicación que funcione como una especie de calculadora
+
+# calculadora_operaciones_v1.py:
+
+# Se trata de una aplicación interactiva
+
+# Se pide un  número
+# Se pide operación para hacer con ese número (suma, resta, multiplicación y división) mayúscula o minuscula.
+# Se pide un segundo número
+# Si al elegir la operación pone otra cosa distinta a (suma, resta, multiplicación y división), invita a volver a ejecutarlo.
+# Si la operación está bien escrita, se realiza la operación deseada entre esos dos números y se muestra el resultado
+
+# Luego, ese resultado, va a ser el primer número con el que realizar otra operación
+# Se pide otra operación para realizar con el resultado .
+# Se pide, eso sí, el otro número
+
+# La aplicación se realimenta de este modo hasta que se ponga FIN de cualquier manera (FIN; FiN, fin, ...), con la que saldrá de la aplicación solo cuando se pide la operación.
+
+# calculadora_operaciones_v2.py:
+
+# Cuando hayan terminado hacer el mismo ejercicio pero que puedan poner FIN en cualquier pregunta y pueda salir
+
+# La aplicación hecha no va ser correcta y no va a controlar excepciones de errores, como por ejemplo, introducir un string dentro de una variable número,si quieres puedes incluirlo
